c3a947d4bf1412701d0067ef2c6daaf80a976745
Arash Haji-Hassanzadeh <56742302+arashhaji@users.noreply.github.com>
2020-08-05 22:03:19 -0400

    Task Manager Complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import tasks from "./reducers";
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const store = createStore(tasks);
+
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 
