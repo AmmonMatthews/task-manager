e12a4b703a44d72a4ae6192be0c43a50cfb36fb2
Arash Haji-Hassanzadeh <56742302+arashhaji@users.noreply.github.com>
2020-08-14 01:39:11 -0400

    register and login completed talking to the backend

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,54 +1,69 @@
-import { EDIT_TASK, CREATE_TASK, REMOVE_TASK } from "./types"
-import {
-    LOGIN_START,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL
-} from "./types"
-import {
-    REGISTER_START,
-    REGISTER_SUCCESS,
-    REGISTER_FAIL
-} from "./types"
+import { EDIT_TASK, CREATE_TASK, REMOVE_TASK } from "./types";
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from "./types";
+import { REGISTER_START, REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
 import axios from "axios";
-import uuid from "react-uuid"
+import uuid from "react-uuid";
 
 export const editTask = (id, params = {}) => {
-    return {
-        type: EDIT_TASK,
-        payload: {
-            id,
-            params,
-        },
-    };
+  return {
+    type: EDIT_TASK,
+    payload: {
+      id,
+      params,
+    },
+  };
 };
 
 export const createTask = ({ title, description }) => {
-    return {
-        type: CREATE_TASK,
-        payload: {
-            id: uuid(),
-            title,
-            description,
-            status: "Unstarted",
-        },
-    };
+  return {
+    type: CREATE_TASK,
+    payload: {
+      id: uuid(),
+      title,
+      description,
+      status: "Unstarted",
+    },
+  };
 };
 
 export const removeTask = (id) => {
-    return {
-        type: REMOVE_TASK,
-        id,
-    };
+  return {
+    type: REMOVE_TASK,
+    id,
+  };
 };
 
-export const getLoggedIn = cred => dispatch => {
-    dispatch({ type: LOGIN_START })
-    return axios
-        .post("https://task-7.herokuapp.com/auth/login", cred, { withCredentials: true })
-        .then((response) => {
-            console.log(response)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-}
+export const getLoggedIn = (cred) => (dispatch) => {
+  dispatch({ type: LOGIN_START });
+  return (
+    axios
+      // .post("https://task-7.herokuapp.com/auth/login", cred, { withCredentials: true })
+      .post("http://localhost:5000/auth/login", cred, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+        dispatch({ type: LOGIN_SUCCESS, payload: response });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: LOGIN_FAIL, payload: err });
+      })
+  );
+};
+export const getRegistered = (cred) => (dispatch) => {
+  dispatch({ type: REGISTER_START });
+  return (
+    axios
+      // .post("https://task-7.herokuapp.com/auth/login", cred, { withCredentials: true })
+      .post("http://localhost:5000/auth/register", cred)
+      .then((response) => {
+        console.log(response);
+        dispatch({ type: REGISTER_SUCCESS, payload: response });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: REGISTER_FAIL, payload: err });
+      })
+  );
+};
--- src/components/register.js
+++ src/components/register.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { getRegistered } from "../actions/index.js";
 // import loginImg from "";
 
-export default function Register(props) {
+const initialState = {
+  username: "",
+  password: "",
+  first_name: "",
+  last_name: "",
+  email: ""
+
+}
+
+
+ function Register(props) {
+
+  const [state, setState] = useState(initialState)
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+   const handelSubmit = (e) => {
+     e.preventDefault()
+     props.getRegistered(state)
+     setState(initialState)
+     props.history.push("/login")
+     
+
+  }
   return (
     <div className="base-container">
       <div className="header">Register</div>
@@ -11,22 +37,61 @@ export default function Register(props) {
       <div className="form">
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" placeholder="username" />
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" placeholder="email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="first_name"
+            name="first_name"
+            placeholder="first_name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="last_name"
+            name="last_name"
+            placeholder="last_name"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
         </div>
       </div>
       <div className="footer">
-        <button type="button" className="btn">
-          Login
+        <button onClick={handelSubmit} type="button" className="btn">
+          Register
         </button>
       </div>
     </div>
   );
-}
+ }
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps, {getRegistered})(Register);
